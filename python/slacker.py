291d8c6d905d67a3f9718833c89cdce90ee4979b
April Jones <april.jones@readytalk.com>
2018-02-27 15:55:22 -0700

    pass in slack API token as environment variable instead

--- python/slacker.py
+++ python/slacker.py
@@ -1,38 +1,15 @@
 #!/usr/bin/python
 
-import re
-import time
-import json
+import os
 from slackclient import SlackClient
 
-# when i have time, instead slack_client will read from ../secrets/slack.key
-slack_client = SlackClient("hint, don't publish your slack api key")
+slack_token = os.environ["SLACK_BOT_TOKEN"]
 
-user_list = slack_client.api_call("users.list")
-for user in user_list.get('members'):
-    if user.get('name') == "gohan-time":
-        slack_user_id = user.get('id')
-        break
+sc = SlackClient(slack_token)
 
-if slack_client.rtm_connect():
-    print "Connected!"
-
-    while True:
-        for message in slack_client.rtm_read():
-            if 'text' in message and message['text'].startswith("<@%s>" % slack_user_id):
-
-                print "Message received: %s" % json.dumps(message, indent=2)
-
-                message_text = message['text'].\
-                        split("<@$s>" % slack_user_id)[1].\
-                        strip()
-
-                if message_text:
-                    slack_client.api_call(
-                       "chat.postMessage",
-                       channel=message['channel'],
-                       text="Oh?",
-                       as_user=True)
-    time.sleep(1)
-                
+sc.api_call(
+        "chat.postMessage",
+        channel="#random",
+        text="Testing!"
+        )
 
