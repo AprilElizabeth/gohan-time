560007b6806fae56d2077fe196cd0e0033103531
AprilElizabeth <april@turtlemadness.net>
2018-03-16 11:13:09 -0600

    I made a terrible mess of this

--- python/slackerex.py
+++ python/slackerex.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import time
-import re
 import requests
-import signal
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 
@@ -19,6 +17,7 @@ GPIO.setup(green, GPIO.OUT)
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
 # Initial states for LEDs
 GPIO.output(green, GPIO.HIGH)
 GPIO.output(red, GPIO.LOW)
@@ -32,17 +31,13 @@ def shutter(button):
     camera.resolution = (1920, 1080)
     GPIO.output(red, GPIO.HIGH)
     GPIO.output(green, GPIO.LOW)
-    time.sleep(2)
+    #time.sleep(2)
     print("Taking picture\n")
     camera.capture('/home/april/gohan-time/images/oishii.jpg')
     camera.close()
     GPIO.output(red, GPIO.LOW)
     GPIO.output(green, GPIO.HIGH)
     post_image()
-    return
-
-def callback(button):
-    print("CallbacK!")
 
 def post_image():
     tasty_treat = {
@@ -51,27 +46,31 @@ def post_image():
     payload={
             "filename":"oishii.jpg",
             "token":token,
-            "channels":['#griz_test_chan'],
+            "channels":['#random'],
+            "initial_comment":"There's food in the breakroom, come and get it!",
+            "title":"美味しい食べ物なの?",
             }
     r = requests.post("https://slack.com/api/files.upload", params=payload, files=tasty_treat)
-    print("Uploaded")
-    print(r.status_code)
-    return None
-
-def handleSIG():
-    GPIO.cleanup()
-signal.signal(signal.SIGINT, handleSIG)
+    #print("Uploaded")
+    #print(r.status_code)
 
 def cleanup():
     GPIO.cleanup()
     print("Cleaning up GPIO...sayonara.")
 
-GPIO.add_event_detect(button, GPIO.RISING, callback=shutter, bouncetime=200)
+GPIO.add_event_detect(button, GPIO.RISING, callback=shutter, bouncetime=10000)
 
 while True:
-    keypressed = raw_input('Press q to quit: ')
-    if keypressed == 'q':
-        cleanup()
-        break
-    else:
-        print("idk")
+    time.sleep(0.01)
+
+'''
+if __name__ == "__main__":
+    while True:
+        keypressed = raw_input('Press q to quit: ')
+        if keypressed == 'q':
+            cleanup()
+            break
+        else:
+            print("idk")
+'''
+
